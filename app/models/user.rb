5f961c3319d64020596dea0819578660b976fab5
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2023-12-09 19:59:54 +0500

    add bcrypt for password hashing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, with: /@/
   validates :password_digest, presence: true
